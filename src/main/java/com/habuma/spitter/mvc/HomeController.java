9cbef14ba59ffc38d2a533283217b9b9c0bb350e
ijieun <mz_ljieun@woowafriends.com>
2021-05-31 17:55:52 +0900

    add inject dependency

--- src/main/java/com/habuma/spitter/mvc/HomeController.java
+++ src/main/java/com/habuma/spitter/mvc/HomeController.java
@@ -1,11 +1,26 @@
 package com.habuma.spitter.mvc;
 
+import java.util.Map;
+import javax.inject.Inject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
   public static final int DEFAULT_SPITTLES_PER_PAGE = 25;
+  private SpitterService spitterService;
 
+  @Inject
+  public HomeController(SpitterService spitterService){
+    this.spitterService = spitterService;
+  }
+
+  @RequestMapping(value={"/","/home"}, method=RequestMethod.GET)
+  public String showHomePage(Map<String, Object> model) {
+    model.put("spittles",
+        spitterService.getRecentSpittles(spittlesPerPage));
+    return "home";
+  }
 }
 
