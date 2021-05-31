41bd113dfa5c19cf726cd7b0c46ae1ef5fba7011
ijieun <mz_ljieun@woowafriends.com>
2021-05-31 17:39:57 +0900

    init spring-mvc example

---
+++ src/main/java/com/habuma/spitter/mvc/HomeController.java
@@ -0,0 +1,11 @@
+package com.habuma.spitter.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+  public static final int DEFAULT_SPITTLES_PER_PAGE = 25;
+
+}
+
---
+++ src/test/java/com/habuma/spitter/mvc/HomeControllerTest.java
@@ -0,0 +1,13 @@
+package com.habuma.spitter.mvc;
+
+import static com.habuma.spitter.mvc.HomeController.*;
+
+import org.junit.jupiter.api.Test;
+
+public class HomeControllerTest {
+  @Test
+  public void shouldDisplayRecentSpittles() {
+
+  }
+
+}
